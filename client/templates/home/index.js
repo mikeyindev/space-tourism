8f9dd648008c1671d1209d272d769dd71eba8bd4
m1k3yfoo <m1k3yfoo@gmail.com>
2016-09-18 16:34:30 -0400

    Added Mocha tests

--- client/templates/home/index.js
+++ client/templates/home/index.js
@@ -1,5 +1,7 @@
 Template.homeIndex.helpers({
     featured: function () {
         return Products.featured();
+        // Defined in products.js file. This is what makes {{#each featured}} in
+        // index.html work
     }
 });
